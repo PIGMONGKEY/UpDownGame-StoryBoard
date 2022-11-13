5293bfc2bfe43a4b04c5b187251993d2e2b989cc
LeeWonHee <pigmong02020202@gmail.com>
2022-11-13 19:16:08 +0900

    reset button method

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var minLabel: UILabel!
+    @IBOutlet var maxLabel: UILabel!
+    @IBOutlet var curValueLabel: UILabel!
+    @IBOutlet var curTryLabel: UILabel!
+    
+    var goalValue: Int = Int.random(in: 0...30)
+    var minValue: Int = 0
+    var maxValue: Int = 30
+    var curValue: Int = 15
+    var curTry: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb"), for: .normal)
     }
 
-
+    @IBAction func checkButtonMethod(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetButtonMethod(_ sender: UIButton) {
+        goalValue = Int.random(in: 0...30)
+        minValue = 0
+        maxValue = 30
+        curValue = 15
+        curTry = 0
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minLabel.text = "\(minValue)"
+        maxLabel.text = "\(maxValue)"
+        curValueLabel.text = "\(curValue)"
+        curTryLabel.text = "\(curTry)/5"
+    }
+    
 }
 
